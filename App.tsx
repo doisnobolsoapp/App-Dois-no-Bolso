4fdea6016ca6132b817ed86a47d74a4b44788702
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2025-11-21 00:59:13 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,48 +1,18 @@
-import { useEffect, useState, lazy, Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import { AppData, ViewState, Transaction, Goal, Account, CreditCard, Investment, Property, Debt } from './types';
 import { Layout } from './components/Layout';
 import { Dashboard } from './components/Dashboard';
-// Removido: import { SettingsPanel } from './components/SettingsPanel';
+import { TransactionList } from './components/TransactionList';
+import { Goals } from './components/Goals';
+import { Reports } from './components/Reports';
+import { FinancialCalendar } from './components/FinancialCalendar';
+import { BankList } from './components/BankList';
+import { CreditCardList } from './components/CreditCardList';
+import { InvestmentDashboard } from './components/InvestmentDashboard';
+import { BalanceSheet } from './components/BalanceSheet';
+import { AIChat } from './components/AIChat';
+import { Settings } from './components/Settings';
 import { loadData, saveData, addTransaction, addMultipleTransactions, deleteTransaction, addGoal, updateGoal, addAccount, deleteAccount, addCreditCard, deleteCreditCard, addInvestment, addInvestmentMovement, deleteInvestment, addProperty, deleteProperty, addDebt, deleteDebt, addCustomCategory } from './services/storageService';
-
-// Lazy load dos componentes pesados - CORRIGIDO para named exports
-const TransactionList = lazy(() => 
-  import('./components/TransactionList').then(module => ({ default: module.TransactionList }))
-);
-const Goals = lazy(() => 
-  import('./components/Goals').then(module => ({ default: module.Goals }))
-);
-const Reports = lazy(() => 
-  import('./components/Reports').then(module => ({ default: module.Reports }))
-);
-const FinancialCalendar = lazy(() => 
-  import('./components/FinancialCalendar').then(module => ({ default: module.FinancialCalendar }))
-);
-const BankList = lazy(() => 
-  import('./components/BankList').then(module => ({ default: module.BankList }))
-);
-const CreditCardList = lazy(() => 
-  import('./components/CreditCardList').then(module => ({ default: module.CreditCardList }))
-);
-const InvestmentDashboard = lazy(() => 
-  import('./components/InvestmentDashboard').then(module => ({ default: module.InvestmentDashboard }))
-);
-const BalanceSheet = lazy(() => 
-  import('./components/BalanceSheet').then(module => ({ default: module.BalanceSheet }))
-);
-const AIChat = lazy(() => 
-  import('./components/AIChat').then(module => ({ default: module.AIChat }))
-);
-const Settings = lazy(() => 
-  import('./components/Settings').then(module => ({ default: module.Settings }))
-);
-
-// Loading component
-const LoadingSpinner = () => (
-  <div className="flex justify-center items-center h-64">
-    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-brand-500"></div>
-  </div>
-);
 
 function App() {
   const [data, setData] = useState<AppData>(loadData());
@@ -160,110 +130,101 @@ function App() {
   };
 
   const renderCurrentView = () => {
-    const viewComponent = (() => {
-      switch (currentView) {
-        case 'DASHBOARD':
-          return <Dashboard data={data} onViewChange={setCurrentView} />;
-        case 'TRANSACTIONS':
-          return (
-            <TransactionList 
-              data={data}
-              onAddTransaction={handleAddTransaction}
-              onAddMultipleTransactions={handleAddMultipleTransactions}
-              onDeleteTransaction={handleDeleteTransaction}
-              onAddCategory={handleAddCategory}
-            />
-          );
-        case 'GOALS':
-          return (
-            <Goals 
-              goals={data.goals}
-              onAddGoal={handleAddGoal}
-              onUpdateGoal={handleUpdateGoal}
-            />
-          );
-        case 'REPORTS':
-          return <Reports data={data} />;
-        case 'CALENDAR':
-          return (
-            <FinancialCalendar 
-              data={data}
-              onAddTransaction={handleAddTransaction}
-              onDeleteTransaction={handleDeleteTransaction}
-              currentUserId="user1" // You can implement user management later
-            />
-          );
-        case 'BANKS':
-          return (
-            <BankList 
-              accounts={data.accounts}
-              onAddAccount={handleAddAccount}
-              onDeleteAccount={handleDeleteAccount}
-              onUpdateAccount={(account: Account) => {
-                // For now, delete and re-add since we don't have updateAccount
-                handleDeleteAccount(account.id);
-                handleAddAccount(account);
-              }}
-            />
-          );
-        case 'CARDS':
-          return (
-            <CreditCardList 
-              cards={data.creditCards}
-              onAddCreditCard={handleAddCreditCard}
-              onDeleteCreditCard={handleDeleteCreditCard}
-              onUpdateCreditCard={(card: CreditCard) => {
-                handleDeleteCreditCard(card.id);
-                handleAddCreditCard(card);
-              }}
-            />
-          );
-        case 'INVESTMENTS':
-          return (
-            <InvestmentDashboard 
-              data={data}
-              onAddInvestment={handleAddInvestment}
-              onAddMovement={handleAddInvestmentMovement}
-              onDeleteInvestment={handleDeleteInvestment}
-              onAddTransaction={handleAddTransaction}
-            />
-          );
-        case 'BALANCE':
-          return (
-            <BalanceSheet 
-              data={data}
-              onAddProperty={handleAddProperty}
-              onDeleteProperty={handleDeleteProperty}
-              onAddDebt={handleAddDebt}
-              onDeleteDebt={handleDeleteDebt}
-            />
-          );
-        case 'CHAT':
-          return (
-            <AIChat 
-              data={data}
-              onAddTransaction={handleAddTransaction}
-              onAddGoal={handleAddGoal}
-              onAddInvestment={handleAddInvestment}
-            />
-          );
-        case 'SETTINGS':
-          return (
-            <Settings 
-              data={data}
-              onDataUpdate={setData}
-            />
-          );
-        default:
-          return <Dashboard data={data} onViewChange={setCurrentView} />;
-      }
-    })();
-
-    return (
-      <Suspense fallback={<LoadingSpinner />}>
-        {viewComponent}
-      </Suspense>
-    );
+    switch (currentView) {
+      case 'DASHBOARD':
+        return <Dashboard data={data} onViewChange={setCurrentView} />;
+      case 'TRANSACTIONS':
+        return (
+          <TransactionList 
+            data={data}
+            onAddTransaction={handleAddTransaction}
+            onAddMultipleTransactions={handleAddMultipleTransactions}
+            onDeleteTransaction={handleDeleteTransaction}
+            onAddCategory={handleAddCategory}
+          />
+        );
+      case 'GOALS':
+        return (
+          <Goals 
+            goals={data.goals}
+            onAddGoal={handleAddGoal}
+            onUpdateGoal={handleUpdateGoal}
+          />
+        );
+      case 'REPORTS':
+        return <Reports data={data} />;
+      case 'CALENDAR':
+        return (
+          <FinancialCalendar 
+            data={data}
+            onAddTransaction={handleAddTransaction}
+            onDeleteTransaction={handleDeleteTransaction}
+            currentUserId="user1"
+          />
+        );
+      case 'BANKS':
+        return (
+          <BankList 
+            accounts={data.accounts}
+            onAddAccount={handleAddAccount}
+            onDeleteAccount={handleDeleteAccount}
+            onUpdateAccount={(account: Account) => {
+              handleDeleteAccount(account.id);
+              handleAddAccount(account);
+            }}
+          />
+        );
+      case 'CARDS':
+        return (
+          <CreditCardList 
+            cards={data.creditCards}
+            onAddCreditCard={handleAddCreditCard}
+            onDeleteCreditCard={handleDeleteCreditCard}
+            onUpdateCreditCard={(card: CreditCard) => {
+              handleDeleteCreditCard(card.id);
+              handleAddCreditCard(card);
+            }}
+          />
+        );
+      case 'INVESTMENTS':
+        return (
+          <InvestmentDashboard 
+            data={data}
+            onAddInvestment={handleAddInvestment}
+            onAddMovement={handleAddInvestmentMovement}
+            onDeleteInvestment={handleDeleteInvestment}
+            onAddTransaction={handleAddTransaction}
+          />
+        );
+      case 'BALANCE':
+        return (
+          <BalanceSheet 
+            data={data}
+            onAddProperty={handleAddProperty}
+            onDeleteProperty={handleDeleteProperty}
+            onAddDebt={handleAddDebt}
+            onDeleteDebt={handleDeleteDebt}
+          />
+        );
+      case 'CHAT':
+        return (
+          <AIChat 
+            data={data}
+            onAddTransaction={handleAddTransaction}
+            onAddGoal={handleAddGoal}
+            onAddInvestment={handleAddInvestment}
+          />
+        );
+      case 'SETTINGS':
+        return (
+          <Settings 
+            data={data}
+            onDataUpdate={setData}
+          />
+        );
+      default:
+        return <Dashboard data={data} onViewChange={setCurrentView} />;
+    }
   };
 
   return (
