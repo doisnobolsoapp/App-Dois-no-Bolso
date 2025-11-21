4b7df165a47fbc327361796b0328af70e65f5ba5
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2025-11-21 00:57:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: 'dist/stats.html',
-      open: false,
-      gzipSize: true,
-      brotliSize: true
-    })
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
-    chunkSizeWarningLimit: 800, // Aumenta um pouco o limite
+    chunkSizeWarningLimit: 800,
   }
 })
