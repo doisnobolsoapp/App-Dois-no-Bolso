30decbefd40dbd103bd6a2397a3df6373f1e710a
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2025-11-22 22:43:21 -0300

    Update PWAInstallPrompt.tsx

--- components/PWAInstallPrompt.tsx
+++ components/PWAInstallPrompt.tsx
@@ -1,6 +1,5 @@
-// src/components/PWAInstallPrompt.tsx
 import React from 'react';
 
 export const PWAInstallPrompt: React.FC = () => {
-  return null; // Componente vazio temporariamente
+  return null;
 };
