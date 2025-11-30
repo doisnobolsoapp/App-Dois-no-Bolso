746f67c684362b419020607a1614775b114376fa
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2025-11-30 08:01:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true, // Permite acesso externo
-    cors: true, // Habilita CORS
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false, // Desativa sourcemaps para simplificar
-  },
-  // Força o Vite a servir os arquivos corretamente
-  appType: 'spa',
+  }
 })
