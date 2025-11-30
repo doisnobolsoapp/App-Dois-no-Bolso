4a27fb11660050e2f540d7b9bc7bdd446cf3ae5c
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2025-11-30 07:32:42 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js (na RAIZ do projeto)
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,17 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          50: '#f0f9ff',
-          100: '#e0f2fe', 
-          500: '#0ea5e9',
-          600: '#0284c7',
-          700: '#0369a1',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
